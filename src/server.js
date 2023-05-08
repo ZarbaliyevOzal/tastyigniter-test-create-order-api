71c232cde0f760ae9fb3fa33a942b57d2494b3d4
Ozal Zarbaliyev <zarbaliyevozal@gmail.com>
2023-05-08 22:33:04 +0400

    first commit

---
+++ src/server.js
@@ -0,0 +1,35 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const fs = require('fs');
+
+app.use(express.json());
+
+app.post('/orders', (req, res) => {
+  const body = JSON.stringify(req.body);
+
+  if (!body) {
+    return res.status(200)
+      .json({ success: false, message: "Body is empty"});
+  }
+
+  if (body) {
+    fs.appendFileSync(__dirname + "/tmp/test.txt",
+      new Date() + "\n" + body + "\n"
+      + "\n\n\n\n\n\n\n\n\n\n\n");
+  }
+
+  return res.status(200).json({
+    success: true,
+    message: "Successfully recorded request"
+  });
+
+});
+
+app.get('/', (req, res) => {
+  return res.status(200).send('OK');
+});
+
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example app listening on port ${process.env.APP_PORT}`);
+})
